4331e60d925a9f9fe81798c094ca1bebeed9dbba
ObjectifGuitare <sebastien@DESKTOP-5GSLS9M.localdomain>
2022-06-13 22:32:52 +0200

    end of the day

--- index.mjs
+++ index.mjs
@@ -5,3 +5,7 @@ const app = express();
 
 app.use(express.static("./public"))
 app.listen(PORT, () => console.log(`Server start: http://localhost:${PORT}/`));
+
+app.get("/api/register", (req, res) =>{
+    res.send("bonjour")
+})
